2e1578f705a03f4b262d4a4deff16e1238b171ac
mehwishTevno <65394112+mehwishTevno@users.noreply.github.com>
2020-05-15 15:18:36 +0500

    Update and rename zeroboy.py to TeevNo.py

--- TeevNo.py
+++ TeevNo.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - MEHWISH -OFFICAL------•◈•
+|__\~~) •||•TEEVNO- MEHWISH---------------•◈•
+|__(-----\  •◈•------MEHWISH-QUEEN--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mMehwish TeevNo
+\033[1;96mInstagram \033[1;93m: \033[1;FlowQueen
+\033[1;96mFacebook  \033[1;93m: \033[1; MEHWISH
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/TheTeevno/TeevNo
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mQueen Mehwish
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowQueen
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Queen4072
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/TheQueen/TeevNo
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Mehwish"
+CorrectPassword = "TeevNo"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +100,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        
 
 def login():
 	os.system('clear')
